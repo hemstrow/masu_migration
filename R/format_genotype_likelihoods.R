10d95ec58e4ab4eca28c7c15bb2cd41bfbec135c
hemstrow <whemstrom@ucdavis.edu>
2020-05-19 14:09:45 -0700

    added a get expected count function, fixed the other function a bit.

--- R/format_genotype_likelihoods.R
+++ R/format_genotype_likelihoods.R
@@ -17,13 +17,13 @@ format_genotype_likelihoods <- function(genotypes){
   genotypes <- data.table::melt(genotypes, id.vars = c("marker", "allele1", "allele2"))
   
   # add genotype letters
-  a.tab <- data.frame(a = c("A", "C", "G", "T"), n = 0:3)
-  genotypes$allele1 <- a.tab$a[match(genotypes$allele1, a.tab$n)]
-  genotypes$allele2 <- a.tab$a[match(genotypes$allele2, a.tab$n)]
+  #a.tab <- data.frame(a = c("A", "C", "G", "T"), n = 0:3)
+  #genotypes$allele1 <- a.tab$a[match(genotypes$allele1, a.tab$n)]
+  #genotypes$allele2 <- a.tab$a[match(genotypes$allele2, a.tab$n)]
   genotypes$genotype <- substr(genotypes$variable, 1, 3)
-  genotypes$genotype[genotypes$genotype == "hoM"] <- paste0(genotypes$allele1[genotypes$genotype == "hoM"], genotypes$allele1[genotypes$genotype == "hoM"])
-  genotypes$genotype[genotypes$genotype == "hom"] <- paste0(genotypes$allele2[genotypes$genotype == "hom"], genotypes$allele2[genotypes$genotype == "hom"])
-  genotypes$genotype[genotypes$genotype == "het"] <- paste0(genotypes$allele1[genotypes$genotype == "het"], genotypes$allele2[genotypes$genotype == "het"])
+  #genotypes$genotype[genotypes$genotype == "hoM"] <- paste0(genotypes$allele1[genotypes$genotype == "hoM"], genotypes$allele1[genotypes$genotype == "hoM"])
+  #genotypes$genotype[genotypes$genotype == "hom"] <- paste0(genotypes$allele2[genotypes$genotype == "hom"], genotypes$allele2[genotypes$genotype == "hom"])
+  #genotypes$genotype[genotypes$genotype == "het"] <- paste0(genotypes$allele1[genotypes$genotype == "het"], genotypes$allele2[genotypes$genotype == "het"])
   
   # pull out individual ids
   genotypes$ind <- gsub(".+_", "", genotypes$variable)
@@ -31,4 +31,39 @@ format_genotype_likelihoods <- function(genotypes){
   # cast and return
   return(data.table::dcast(genotypes[,-c("allele1", "allele2", "variable")], 
                            formula = ind ~ marker + genotype, value.var = "value"))
+}
+
+
+get_expected_counts <- function(genotypes, method = "af"){
+  genotypes[genotypes == 0.333333] <- NA
+  counts <- t(t(genotypes[, -c(1:3)]) * c(0, 1, 2))
+  counts <- as.data.table(counts)
+  data.table::set(genotypes, j = 4:ncol(genotypes), value = counts)
+  
+  genotypes <- data.table::melt(genotypes, id.vars = c("marker", "allele1", "allele2"))
+  genotypes <- data.table::dcast(genotypes[, -c("allele1", "allele2")], variable ~ marker, fun.aggregate = sum)
+  
+  inds <- genotypes[,1]
+  genotypes <- genotypes[,-1]
+  af <- colMeans(genotypes, na.rm = T) # this is the allele frequency of the "1" allele
+  
+  # identify all of the NAs and the columns that they belong to
+  NAs <- which(is.na(genotypes)) # cells with NA
+  NA.cols <- floor(NAs/nrow(genotypes)) + 1 # figure out the column
+  adj <- which(NAs%%nrow(genotypes) == 0) # adjust for anything that sits in the last row, since it'll get assigned the wrong column
+  NA.cols[adj] <- NA.cols[adj] - 1
+  
+  # do interpolation for each missing data point
+  if(method == "bernoulli"){
+    ndat <- rbinom(length(NAs), 2, af[NA.cols])
+  }
+  else if(method == "af"){
+    ndat <- af[NA.cols]
+  }
+  
+  # replace
+  genotypes <- as.matrix(genotypes)
+  genotypes[NAs] <- ndat
+  
+  return(data.table::as.data.table(genotypes))
 }
